dd55961a1872cb3f71119c347fb2ff1070200e17
hayashi takaharu <takaharu.hayashi@gu3.co.jp>
2013-11-24 22:11:53 +0900

    test.html修正

--- scrip.rb
+++ scrip.rb
@@ -8,10 +8,10 @@ file = File.open("test.html")
 doc = Nokogiri::HTML(file)
 
 def mapping(chil)
-  if chil.children.size == 0
-  else
+  #TODO:クラスオブジェクトを返すようにする
+  unless chil.children.size == 0
     #TODO:Size == 1ではなく、最後の子だと判定したい。。。
-    if chil.children.size == 1
+    if chil.children.size == 1 && chil.children.first.children.size == 0
       puts "#{chil.children}::#{chil.children.class}"
     end
     chil.children.each {|ccc|
