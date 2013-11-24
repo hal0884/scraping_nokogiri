41e6675121128eed10b5776c780c5154d7ed1c2a
hayashi takaharu <takaharu.hayashi@gu3.co.jp>
2013-11-24 22:34:59 +0900

    class っぽく

--- scrip.rb
+++ scrip.rb
@@ -1,22 +1,35 @@
 require 'rubygems'
 require 'bundler/setup'
+
 require 'nokogiri'
 
 file = File.open("test.html")
 doc = Nokogiri::HTML(file)
 
-def mapping(chil)
-  #TODO:クラスオブジェクトを返すようにする
-  unless chil.children.size == 0
-    #TODO:Size == 1ではなく、最後の子だと判定したい。。。
-    if chil.children.size == 1 && chil.children.first.children.size == 0
-      puts "#{chil.children}::#{chil.children.class}"
+class NScraping
+  attr_reader :ans
+
+  def initialize()
+    @ans = []
+  end
+
+  def mapping(chil)
+    #TODO:クラスオブジェクトを返すようにする
+    unless chil.children.size == 0
+      #TODO:Size == 1ではなく、最後の子だと判定したい。。。
+      if chil.children.size == 1 && chil.children.first.children.size == 0
+        @ans.push(chil.children.text)
+      end
+      chil.children.each {|ccc|
+        mapping(ccc)
+      }
     end
-    chil.children.each {|ccc|
-      mapping(ccc)
-    }
   end
 end
 
-mapping(doc.xpath('//body'))
+ns = NScraping.new()
+aaa = ns.mapping(doc.xpath('//body'))
+puts aaa
+puts ns.ans
+puts ns.ans.class
 
