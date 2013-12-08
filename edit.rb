565b8e2a91b4d31ccb0511e893458e75b1fdccc2
hayashi takaharu <takaharu.hayashi@gu3.co.jp>
2013-12-08 23:23:39 +0900

    edit.rb

---
+++ edit.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'kconv'
+require 'nokogiri'
+
+
+
+def strip(src, url = nil)
+
+  src = src.toutf8.chars.map{ |s|
+    s.valid_encoding? ? s : "\uFFFD"
+  }.join("")
+
+  dom = begin
+    Nokogiri::HTML.parse(src, url, "UTF-8")
+  rescue
+    retry
+  end
+
+  dom.xpath('//style|//script').each{ |n|
+    n.unlink
+  }
+
+  dom.xpath('//text()').each do |t|
+    if t.content.strip.length > 0
+      puts t.content.strip
+    end
+  end
+  #ans = dom.xpath('//text()').map{ |n|
+  #  n.content
+  #}.join(' ').gsub(/\p{WSpace}+/,' ').strip
+  #return ans
+end
+
+
+file = File.open("test.html")
+doc = Nokogiri::HTML(file)
+strip(doc.inner_html)
